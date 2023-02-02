de70089418c0df1531e17c64a6b450e86b8dfaff
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-02-02 14:42:52 +0100

    Synchronisation of the audio when scene loaded

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioLoadScene;
+    [SerializeField] private AudioSource _audioExitGame;
+
     public void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
     }
 
+    public void InvokeLoadScene(int index)
+    {
+        _audioLoadScene.Play();
+        StartCoroutine(WaitAndLoadScene_Coroutine(index, 1f));
+    }
+
+    private IEnumerator WaitAndLoadScene_Coroutine(int index, float time)
+    {
+        yield return new WaitForSeconds(time);
+        LoadScene(index);
+    }
+
     public void ExitGame()
     {
+        _audioExitGame.volume = 1f;
+        _audioExitGame.Play();
         Application.Quit();
         Debug.Log("Exit pressed");
     }
