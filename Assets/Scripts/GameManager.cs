d7ab0259816b4a4661becd837fe547785f081819
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-02-02 11:19:59 +0100

    Add basic UI menu for level0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(int index)
     {
-        
+        SceneManager.LoadScene(index);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Exit pressed");
     }
 }
