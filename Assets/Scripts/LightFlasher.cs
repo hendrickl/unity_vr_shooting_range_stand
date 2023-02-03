2a47b8079b50d868af943fe1e3e2b403735ca67d
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-02-03 15:44:58 +0100

    Add green flashing light behavior on collision

--- Assets/Scripts/LightFlasher.cs
+++ Assets/Scripts/LightFlasher.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class LightFlasher : MonoBehaviour
 {
     private bool _lightIsOn = false;
-    [SerializeField] private Light _light;
+    private int _currentIndex = 0;
+    [SerializeField] private Light[] _lights = new Light[10];
 
     private void OnCollisionEnter(Collision other)
     {
         if (gameObject.CompareTag("BodyTarget"))
         {
-            _lightIsOn = !_lightIsOn;
-            _light.color = Color.green;
-            _light.gameObject.SetActive(_lightIsOn);
+            if (_currentIndex < _lights.Length)
+            {
+                _lightIsOn = !_lightIsOn;
+                _lights[_currentIndex].color = Color.green;
+                _lights[_currentIndex].gameObject.SetActive(_lightIsOn);
+                _currentIndex++;
+            }
+            _lightIsOn = false;
         }
-        _lightIsOn = false;
     }
 }
